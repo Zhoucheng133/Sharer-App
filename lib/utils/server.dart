bc92ce8bca566ff237b5f646a2e6e77d4a31c868
zhoucheng <zhoucheng133@outlook.com>
2025-03-30 16:59:54 +0800

    链接到动态库

--- lib/utils/server.dart
+++ lib/utils/server.dart
@@ -1,40 +1,82 @@
+import 'dart:ffi';
 import 'dart:io';
+import 'dart:isolate';
 
+import 'package:ffi/ffi.dart';
 import 'package:flutter/services.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
 import "package:path/path.dart" as p;
-import 'package:process_run/process_run.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+typedef StartServer = void Function(Pointer<Utf8>, Pointer<Utf8>, Pointer<Utf8>, Pointer<Utf8>, Pointer<Utf8>);
+typedef StartServerFunc = Void Function(Pointer<Utf8>, Pointer<Utf8>, Pointer<Utf8>, Pointer<Utf8>, Pointer<Utf8>);
+
+typedef StopServer=int Function();
+typedef StopServerFunc=Int32 Function();
+
 
 class Server {
 
-  late String corePath;
-  late Shell shell;
+  late String webPath;
+  late DynamicLibrary dynamicLib;
+  late StopServer stopServer;
 
   init() async {
+
+    
+    dynamicLib=DynamicLibrary.open(Platform.isMacOS ? 'libserver.dylib' : 'libserver.dll');
+    stopServer=dynamicLib
+    .lookup<NativeFunction<StopServerFunc>>('StopServer')
+    .asFunction();
+    
+
     String supportDir=(await getApplicationSupportDirectory()).path;
-    corePath=p.join(supportDir, Platform.isWindows ? "core.exe" : "core");
-    final file = File(corePath);
-    if(!file.existsSync()){
-      final ByteData data = await rootBundle.load(Platform.isWindows ? "assets/core.exe" : "assets/core");
-      final buffer = data.buffer.asUint8List();
-      await file.writeAsBytes(buffer);
+    webPath=p.join(supportDir, "dist");
+
+    List<String> assetFiles = [
+      "assets/dist/index.html",
+      "assets/dist/vite.svg",
+      "assets/dist/assets/index-BGsdZcFV.css",
+      "assets/dist/assets/index-DRqKvH6v.js",
+      "assets/dist/assets/primeicons-C6QP2o4f.woff2",
+      "assets/dist/assets/primeicons-DMOk5skT.eot",
+      "assets/dist/assets/primeicons-Dr5RGzOO.svg",
+      "assets/dist/assets/primeicons-MpK4pl85.ttf",
+      "assets/dist/assets/primeicons-WjwUDZjB.woff",
+    ];
+
+    Directory webDir = Directory(webPath);
+
+    final pref=await SharedPreferences.getInstance();
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    final prefsVersion=pref.getString("version");
+    final version=packageInfo.version;
+
+    if(webDir.existsSync()){
+      if(prefsVersion!=version){
+        webDir.deleteSync(recursive: true);
+        pref.setString("version", version);
+      }
     }
-    if (!Platform.isWindows) {
-      await Process.run('chmod', ['+x', corePath]);
+
+    if(!webDir.existsSync()){
+
+      for (String assetPath in assetFiles) {
+        final file = File(p.join(supportDir, p.relative(assetPath, from: "assets")));
+        final ByteData data = await rootBundle.load(assetPath);
+        final buffer = data.buffer.asUint8List();
+        final localDir=Directory(p.join(supportDir, p.dirname(p.relative(assetPath, from: "assets"))));
+        if(!localDir.existsSync()){
+          await localDir.create(recursive: true);
+        }
+        await file.writeAsBytes(buffer);
+      }
     }
-    shell=Shell();
   }
 
   Server(){
     init();
-  }
-
-  String getCmd(String port, String dir, String username, String password){
-    String cmd="'$corePath' -port $port -d '$dir'";
-    if(username.isNotEmpty && password.isNotEmpty){
-      cmd+=" -u $username -p $password";
-    }
-    return cmd;
   }
 
   Future<bool> portCheck(String port) async {
@@ -53,15 +95,22 @@ class Server {
     return directory.existsSync();
   }
 
+  late Isolate isolate;
+  static void isolateFunction(List<String> params){
+    final dynamicLib = DynamicLibrary.open(Platform.isMacOS ? 'libserver.dylib' : 'libserver.dll');
+    StartServer startServer=dynamicLib
+    .lookup<NativeFunction<StartServerFunc>>('StartServer')
+    .asFunction();
+    // port *C.char, basePath *C.char, username *C.char, password *C.char, webPath *C.char
+    startServer(params[0].toNativeUtf8(), params[1].toNativeUtf8(), params[2].toNativeUtf8(), params[3].toNativeUtf8(), params[4].toNativeUtf8());
+  }
+
   Future<void> run(String port, String dir, String username, String password) async {
-    try {
-      await shell.run(getCmd(port, dir, username, password));
-    } on ShellException catch (_) {}
+    isolate=await Isolate.spawn(isolateFunction, [port, dir, username, password, webPath]);
   }
 
   void stop(){
-    try {
-      shell.kill();
-    } catch (_) {}
+    stopServer();
+    isolate.kill();
   }
 }
